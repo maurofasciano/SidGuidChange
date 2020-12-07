1b1bcc07bcb5e83e249bb05f540c80ea4d850243
maurofasciano <60512872+maurofasciano@users.noreply.github.com>
2020-12-07 19:47:48 +0100

    first commit

---
+++ SidGuidChange/Classes/Methods.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Security.Principal;
+using Microsoft.Win32;
+
+namespace SidGuidChange.Methods
+{
+    public class Functions
+    {
+
+        public static void InvalidRequest()
+        {
+            Console.Clear();
+            Console.WriteLine("");
+            Console.WriteLine("|-------------------------------------------------|");
+            Console.WriteLine("|- INVALID Request! Only Selections are accepted -|");
+            Console.WriteLine("|----------- Press ENTER to continue -------------|");
+            Console.WriteLine("|-------------------------------------------------|");
+            Console.WriteLine("");
+            Console.ReadLine();
+        }
+
+        public static void SidGuidRead()
+        {
+            string getSid;
+            using (WindowsIdentity windowsIdentity = WindowsIdentity.GetCurrent())
+            {
+                getSid = windowsIdentity.User.AccountDomainSid.Value.ToString();
+                // (0, windowsIdentity.User.AccountDomainSid.Value.LastIndexOf('-'))
+            }
+
+            object value = Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Cryptography", @"MachineGuid", (object)"defaultValue");
+            RegistryKey keyBase = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+            RegistryKey key = keyBase.OpenSubKey(@"SOFTWARE\Microsoft\Cryptography", RegistryKeyPermissionCheck.ReadSubTree);
+            value = key.GetValue("MachineGuid");
+            if (value != null && value.ToString() != "defaultValue")
+            { 
+                key.Close();
+            }
+
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("|-------------------------------------------------------|");
+            Console.WriteLine($"|Your SiD is :\t" + getSid+"|");
+            Console.WriteLine($"|Your GuiD is :\t" + value+"     |");
+            Console.WriteLine("|-------------------------------------------------------|");
+            Console.WriteLine();
+            Console.WriteLine("Press ENTER to Return ...");
+            Console.ReadLine();
+
+
+        }
+
+        public static void SidChanger()
+        {
+            using (System.Diagnostics.Process pProcess = new System.Diagnostics.Process())
+            {
+                pProcess.StartInfo.FileName = "notepad.exe";
+                pProcess.StartInfo.Arguments = "olaa"; //argument
+                pProcess.StartInfo.UseShellExecute = false;
+                pProcess.StartInfo.RedirectStandardOutput = true;
+                pProcess.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                pProcess.StartInfo.CreateNoWindow = true; //not diplay a windows
+                pProcess.Start();
+                string output = pProcess.StandardOutput.ReadToEnd(); //The output result
+                pProcess.WaitForExit();
+            }
+        }
+
+    }
+}
---
+++ SidGuidChange/Program.cs
@@ -0,0 +1,110 @@
+﻿using SidGuidChange.Methods;
+using System; 
+using Microsoft.Win32;
+
+namespace SidGuidChange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char tm = '\u00A9';
+            Console.Clear();
+            Console.WriteLine("|--------------------------------------|");
+            Console.WriteLine($"|- Developed by Faggiano's Software {tm} -|");
+            Console.WriteLine("|--------------------------------------|");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine($"Welcome to SiD and GuiD Tools, writen and developed by Faggiano for Windows 10 {tm}." +
+                "\nHere you can list your Ids, change them for deploy cloned OS on Active Direcory, and save to file.");
+            Console.WriteLine();
+            Console.WriteLine("IMPORTANT NOTICE !\nRemeber to rename your machine before config changes, so you'll have" +
+                "\na complete list of Ids associated to hostnames.  --- ENJOY! ---");
+            Console.WriteLine();
+            Console.WriteLine("Press ENTER to continue....");
+            Console.ReadLine();
+
+
+            while (true)
+                {
+
+                Console.Clear();
+                Console.WriteLine();
+                Console.WriteLine("|----------------------------------------------|");
+                Console.WriteLine("|---- What'u wanna do with Machine's Ids ? ----|");
+                Console.WriteLine("|----------------------------------------------|");
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("-- SELECT OPTIONS: -- ");
+                Console.WriteLine("|----------------------------------------------|");
+                Console.WriteLine("|------ 1) Read SID & GUID Of your OS ---------|");
+                Console.WriteLine("|------ 2) Change OS' SID  --------------------|");
+                Console.WriteLine("|-------3) Change OS' GUID --------------------|");
+                Console.WriteLine("|-------4) Change Both    ---------------------|");
+                Console.WriteLine("|-------5) Quit Program   ---------------------|");
+                Console.WriteLine("|----------------------------------------------|");
+
+                string risposta = Console.ReadLine();
+                bool rispostaUtente = int.TryParse(risposta, out int rispostaUtenteInt);
+
+                if (rispostaUtente == false)
+                {
+                    Console.Clear();
+
+                    Console.WriteLine("");
+                    Console.WriteLine("|---------------------------------------|");
+                    Console.WriteLine("|- Invalid Args, Integer accepted only -|");
+                    Console.WriteLine("|------ Press ENTER to continue --------|");
+                    Console.WriteLine("|---------------------------------------|");
+                    Console.WriteLine("");
+                    Console.ReadLine();
+
+                    continue;
+
+                }
+
+                switch(rispostaUtenteInt)               
+                {
+                    case 1:
+                        Functions.SidGuidRead();
+                        break;
+
+                    case 2:
+                        Functions.SidChanger();
+                        break;
+
+                    case 3:
+                        break;
+
+                    case 4:
+                        break;
+
+                    case 5:
+                        Console.WriteLine("");
+                        Console.WriteLine("|---------------------------------------|");
+                        Console.WriteLine("|- Selected QUIT, press ENTER to Close -|");
+                        Console.WriteLine("|---------------------------------------|");
+                        Console.WriteLine("");
+                        Console.ReadLine();
+                        Environment.Exit(0);
+                        return;
+
+                    default:
+                        Functions.InvalidRequest();
+                        break;
+
+                }
+                
+
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
